9fe692613fd44d65d0922dcef4a191363214be14
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-02-08 21:43:40 +0530

    sliding window

---
+++ SlidingWindow/N010_L125_BestTimetoBuyandSellStock/SimpleIterativeSolution1.java
@@ -0,0 +1,36 @@
+package SlidingWindow.N010_L125_BestTimeToBuyAndSellStock;
+
+import java.util.Arrays;
+
+class SolutionSimpleIterativeSolution1 {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0)
+            return 0;
+        int[] min = new int[prices.length];
+        min[0] = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            min[i] = Math.min(min[i - 1], prices[i]);
+        }
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - min[i]);
+        }
+        return maxProfit;
+    }
+}
+
+public class SimpleIterativeSolution1 {
+    public static void main(String[] args) {
+        SolutionSimpleIterativeSolution1 solution = new SolutionSimpleIterativeSolution1();
+        int[] nums1 = { 10,1,5,6,7,1 };
+        int[] nums2 = {10,8,7,5,2 };
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : " + Arrays.toString(nums1));
+        System.out.println("Output : " + solution.maxProfit(nums1));
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : " + Arrays.toString(nums2));
+        System.out.println("Output : " + solution.maxProfit(nums2));
+        System.out.println("---------------------------------------------------");
+    }
+
+}
---
+++ SlidingWindow/N010_L125_BestTimetoBuyandSellStock/SimpleIterativeSolution2.java
@@ -0,0 +1,34 @@
+package SlidingWindow.N010_L125_BestTimeToBuyAndSellStock;
+
+import java.util.Arrays;
+
+class SolutionSimpleIterativeSolution2 {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0)
+            return 0;
+        int minimum = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - minimum);
+            minimum = Math.min(minimum, prices[i]);
+        }
+        return maxProfit;
+    }
+}
+
+public class SimpleIterativeSolution2 {
+    public static void main(String[] args) {
+        SolutionSimpleIterativeSolution2 solution = new SolutionSimpleIterativeSolution2();
+        int[] nums1 = { 10,1,5,6,7,1 };
+        int[] nums2 = {10,8,7,5,2 };
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : " + Arrays.toString(nums1));
+        System.out.println("Output : " + solution.maxProfit(nums1));
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : " + Arrays.toString(nums2));
+        System.out.println("Output : " + solution.maxProfit(nums2));
+        System.out.println("---------------------------------------------------");
+    }
+
+}
